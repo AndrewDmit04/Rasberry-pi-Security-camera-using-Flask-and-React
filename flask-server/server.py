0a7c1b12b0466a655a2f0ea86e079f1d8885e095
AndrewDmit04 <144502433+AndrewDmit04@users.noreply.github.com>
2024-01-16 23:47:12 -0600

    Update server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,9 @@
 ##############################################################################################
-#CODE WRITTED BY: Andrew Dmitrievsky
-#PURPOSE: create a React-flask application that could monitor-record any part of the
+#CODE WRITTEN BY: Andrew Dmitrievsky
+#PURPOSE: create a React-flask application that could monitor any part of the
 #house. It will be able to record videos and store them on the server, run facial recognition
 #and store the faces on the server, and will produce a live feed of the camera.
-#It will also be able to show the recorded videos and faces inside of the application
+#It will also be able to show the recorded videos and faces inside the application
 ##############################################################################################
 
 from flask import Flask, Response, jsonify,send_file
@@ -14,7 +14,7 @@ from queue import Queue
 from datetime import datetime
 
 ########setup###########
-SERVER_IP = '192.168.0.157'
+SERVER_IP = 'YOUR IP'
 MAX_FACES_IN_FILE = 200
 MAX_RECORDINGS_IN_FILE = 100
 MAX_RECENT_FACES = 10
@@ -32,7 +32,7 @@ face_buffer = []   #face buffer for all the images of the faces
 def get_datetime(file_name):
     return datetime.strptime(file_name, "%Y:%m:%d::%H:%M")  # Parse 'file_name' string into a datetime object using the specified format
 def sort_times(dates):
-    formated = [file.split("_")[0] for file in dates] #get the all the dates of the images as a list
+    formated = [file.split("_")[0] for file in dates] #get all the dates of the images as a list
     sorted_dates = sorted(formated, key=get_datetime) #sort the list oldest to newest
     return sorted_dates[::-1]   #return the reversed list to get which ones to delete
 
@@ -41,7 +41,7 @@ def delete_old_videos():
     path_to_thumbnails = './Recorded/thumbnails'
     video_files = [file for file in os.listdir(path_to_videos) if file.endswith('.avi')]    #get all the files in the directory
     if len(video_files) > MAX_RECORDINGS_IN_FILE:
-        video_files = [file[:-4] for file in video_files]                                       #get rid of the extention on at the back of the name
+        video_files = [file[:-4] for file in video_files]                                       #get rid of the extension on at the back of the name
         sorted_videos = sort_times(video_files)                                                 #sort all the files based on time
         sorted_videos = [file + ".avi" for file in sorted_videos]                               #put back the extension on all the files
         while len(sorted_videos) > MAX_RECORDINGS_IN_FILE:
@@ -55,7 +55,7 @@ def delete_old_faces():                       #function will delete all old face
     path_to_photos = "./Recorded/faces"       #path to the faces images
     face_photos = [file for file in os.listdir(path_to_photos) if file.endswith('.jpg')]    #get a list of all the images in the file
     if len(face_photos) > max_faces:          #if the length is more than it should be
-        sorted_faces = sort_times(face_photos) #sort the faces based on the date returns oldest last
+        sorted_faces = sort_times(face_photos) #sort the faces based on the date returns the oldest last
         while len(face_photos) > max_faces:   #while the length of the faces array is more than it should be
             remove = sorted_faces.pop()       #pop the last image in the array which is the oldest one
             index = [file.split('_')[0] for file in face_photos].index(remove)  #get the index of the oldest component in the file name array
@@ -63,13 +63,13 @@ def delete_old_faces():                       #function will delete all old face
             os.remove(path_to_photos + "/" + removing)  #remove the photo from the file
 
 def add_faces(frame,faces):         #add faces to the recent faces buffer
-    num_of_faces = 1                #num of faces incase theres more than one face in a frame
-    for(x,y,w,h) in faces:          #get the cordinates of the faces in the frame
+    num_of_faces = 1                #num of faces incase there more than one face in a frame
+    for(x,y,w,h) in faces:          #get the coordinates of the faces in the frame
         face_buffer.append(frame[y-10:y+h+10, x-10:x + w+ 10])  #append the face image inside the buffer plus some margin around the face
         current_date_time = datetime.now()  #get the current dateTime
         cv.imwrite(current_date_time.strftime("./Recorded/faces/%Y:%m:%d::%H:%M") + f'_{num_of_faces}' +'.jpg' ,frame[y-10:y+h+10, x-10:x + w+ 10]) #write the image to the file
-        num_of_faces += 1   #add one to num of faces if more faces then the file will have a diffrent name
-    while len(face_buffer) > MAX_RECENT_FACES:  #if the buffer length is more than it should be keep poping the faces until its right
+        num_of_faces += 1   #add one to num of faces if more faces then the file will have a different name
+    while len(face_buffer) > MAX_RECENT_FACES:  #if the buffer length is more than it should be keep popping the faces until its right
         face_buffer.pop(0)  #pop the first face or the oldest
     delete_old_faces()      #delete all the old faces from the file
    
@@ -89,7 +89,7 @@ frame_queue = Queue(maxsize=10) #set a queue of frames with a max size of 10
 def generate_video_feed():
     cap = cv.VideoCapture(0)                    #get camera with index 0
     fourcc = cv.VideoWriter_fourcc(*'XVID')     #set  video Writer to record in .avi
-    out = cv.VideoWriter(getFileName(cap), fourcc, 20.0, (640, 480))   #set a out to write the frames to
+    out = cv.VideoWriter(getFileName(cap), fourcc, 20.0, (640, 480))   #set an out to write the frames to
     delete_old_videos()             #delete all the old videos
     startTimeRecord = time.time()       #get the recording start time
     startTimeFace = time.time()         #get the first face start time
@@ -160,7 +160,7 @@ def Images():
 def serve_videos():
     path_to_videos = './Recorded/Recordings'
     video_files = [file for file in os.listdir(path_to_videos) if file.endswith('.avi')]    #get all the files in the directory
-    video_files = [file[:-4] for file in video_files]                                       #get rid of the extention on at the back of the name
+    video_files = [file[:-4] for file in video_files]                                       #get rid of the extension on at the back of the name
     sorted_videos = sort_times(video_files)                                                 #sort all the files based on time
     sorted_videos = [file + ".avi" for file in sorted_videos]                               #put back the extension on all the files
     return jsonify(sorted_videos)                                                           #return the sorted file list
@@ -169,7 +169,7 @@ def serve_videos():
 @app.route('/videos/<filename>')
 def serve_video(filename):
     video_path = './Recorded/Recordings/' + filename                                        #get the path to the requested video
-    return send_file(video_path,as_attachment=True, mimetype='video/x-msvideo')             #return the video as an attachment to dowload it for the user
+    return send_file(video_path,as_attachment=True, mimetype='video/x-msvideo')             #return the video as an attachment to download it for the user
 
 #gets the thumbnail for the video
 @app.route('/thumbnails/<filename>')
@@ -196,7 +196,7 @@ def get_face(faceName):
     return send_file(face_path, mimetype='image/jpeg')  #serve the image
 
 if __name__ == '__main__':
-    video_thread = threading.Thread(target=generate_video_feed) #start a thread that constantly records even if the page isnt opened
+    video_thread = threading.Thread(target=generate_video_feed) #start a thread that constantly records even if the page isn't opened
     video_thread.daemon = True  #set daemon to true so the program stops when the main program stops
     video_thread.start()    #start the thread
     app.run(host=SERVER_IP,port=5000)   #start the flask server
